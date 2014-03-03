627a3f4636b7e63f0ffadafc67bd11b340f1fc69
Pavel <xPaw@users.noreply.github.com>
2014-03-03 20:33:14 +0200

    Send git commits to server

--- deploy_bombgame.js
+++ deploy_bombgame.js
@@ -1,26 +1,38 @@
 var hookshot = require('hookshot');
 var exec = require('child_process').exec;
-var Rcon = require('rcon');
+var rcon = require('rcon');
 
-
-hookshot('refs/heads/master', function(info){
-
+hookshot('refs/heads/master', function(info)
+{
     console.log('Pulling & compiling');
 
-
-    exec('bash compile_bombgame.sh', function(error, stdout, stderr){
+    exec('bash compile_bombgame.sh', function(error, stdout, stderr)
+    {
         console.log('stdout: ' + stdout);
         console.log('stderr: ' + stderr);
-        if (error !== null) {
-          console.log('exec error: ' + error);
+
+        if (error !== null)
+        {
+            console.log('exec error: ' + error);
         }
-        var conn = new Rcon('timmw.no-ip.org', 27015, 'test');
-        conn.on('auth', function(error, stdout, stderr) {
-            conn.send('say Received a GitHub hook!;sm plugins reload BombGame');
-                conn.disconnect();
-            });
+        
+        var conn = new rcon('timmw.no-ip.org', 27015, 'test');
+        conn.on('auth', function()
+        {
+            var length = info.commits.length;
+
+            conn.send('say ' + info.pusher.name + ' pushed ' + length + ' commit' + ( length === 1 ? '' : 's' ) + ':;sm plugins reload BombGame');
+
+            for( var i = 0; i < length; i++ )
+            {
+                var commit = info.commits[ i ];
+
+                conn.send( 'say ' + commit.author.name + ' | ' + commit.message );
+            }
+
+            conn.disconnect();
+        });
         conn.connect();
     });
 
-}).listen(1337)
-
+}).listen(1337);

a35d57177c1dacfcb3abb0d863f92e67aede6ae2
Tim Miller-Williams <tim.millerwilliams@gmail.com>
2014-03-04 22:22:29 +0000

    Perform git pull on init

--- deploy_bombgame.js
+++ deploy_bombgame.js
@@ -2,44 +2,61 @@ var hookshot = require('hookshot');
 var exec = require('child_process').exec;
 var rcon = require('rcon');
 
-hookshot('refs/heads/master', function(info)
-{
-    console.log('Pulling & compiling');
+var host = 'timmw.no-ip.org', port = 27015, rconPassword = 'test';
 
-    exec('bash compile_bombgame.sh', function(error, stdout, stderr)
+console.log('Pulling latest changes...');
+
+exec('git pull', function(error, stdout, stderr){
+
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+
+    if (error !== null)
     {
-        console.log('stdout: ' + stdout);
-        console.log('stderr: ' + stderr);
+        console.log('exec error: ' + error);
+    }
 
-        if (error !== null)
+    console.log('Waiting for call from github webhook...');
+    
+    hookshot('refs/heads/master', function(info)
+    {
+        console.log('Pulling & compiling');
+
+        exec('bash compile_bombgame.sh', function(error, stdout, stderr)
         {
-            console.log('exec error: ' + error);
-        }
-        
-        var conn = new rcon('timmw.no-ip.org', 27015, 'test');
+            console.log('stdout: ' + stdout);
+            console.log('stderr: ' + stderr);
 
-        conn.on('auth', function()
-        {
-            conn.send('sm plugins refresh;sm plugins reload BombGame');
-
-            setTimeout(function()
+            if (error !== null)
             {
-                var length = info.commits.length;
+                console.log('exec error: ' + error);
+            }
+            
+            var conn = new rcon(host, port, rconPassword);
 
-                conn.send('say ">> ' + info.pusher.name + ' pushed ' + length + ' commit' + (length === 1 ? '' : 's') + ': ' + info.compare + '"');
+            conn.on('auth', function()
+            {
+                conn.send('sm plugins refresh;sm plugins reload BombGame');
 
-                for(var i = 0; i < length; i++)
+                setTimeout(function()
                 {
-                    var commit = info.commits[ i ];
+                    var length = info.commits.length;
 
-                    conn.send('say ">> ' + commit.id.substring(0, 8) + ' | ' + commit.message + '"');
-                }
+                    conn.send('say ">> ' + info.pusher.name + ' pushed ' + length + ' commit' + (length === 1 ? '' : 's') + ': ' + info.compare + '"');
 
-                conn.disconnect();
-            }, 1111);
+                    for(var i = 0; i < length; i++)
+                    {
+                        var commit = info.commits[ i ];
+
+                        conn.send('say ">> ' + commit.id.substring(0, 8) + ' | ' + commit.message + '"');
+                    }
+
+                    conn.disconnect();
+                }, 1111);
+            });
+
+            conn.connect();
         });
 
-        conn.connect();
-    });
-
-}).listen(1337);
+    }).listen(1337);
+})
